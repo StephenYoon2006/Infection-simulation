01964f0bf31fe07e2bad0a03cd5c95c29520fa28
WonnuBeFriends <59417874+WonnuBeFriends@users.noreply.github.com>
2020-10-24 19:22:26 -0400

    Infection simulation

--- Circle.js
+++ Circle.js
@@ -6,13 +6,20 @@ class Circle{
         this.x = Math.random()*width;
         this.y = Math.random()*height;
         this.state = state
+        if(this.state === "sick"){
+            setTimeout(() =>{
+            this.state = "immune";
+            immuneStat +=1
+            sickStat -=1;
+        }, 5000)
+        };
+        this.touched=[];
     }
 
     draw(){
         if(this.state === "sick"){
             fill("red");
             ellipse(this.x, this.y, CIRCLE_SIZE, CIRCLE_SIZE)
-            
         }
         if(this.state === "healthy"){
             fill("green");
@@ -33,16 +40,35 @@ class Circle{
         if(this.y > height || this.y < 0){
             this.velocityY = -this.velocityY
         }
+        let touching = [];
         for(let i =0; i < circles.length; i++){
             if(Math.sqrt(Math.pow(circles[i].x-this.x,2)+(Math.pow(circles[i].y-this.y,2))) < CIRCLE_SIZE * 2){
-                if(circles[i].state === "sick" && this.state != "immune" && this.state != "sick"){
-                    this.state = "sick";  
-                    if(this.state === "sick"){
+                touching.push(circles[i]);
+                if(circles[i].state === "sick" && this.state != "immune" && this.state != "sick")
+                    if(this.state != "sick"){
+                        if(Math.random() < 0.05){
+                        this.state = "sick";
                         sickStat +=1
                         healthyStat -=1
-                    }                         
-                    }
+                        setTimeout(() =>{
+                            this.state = "immune";
+                            immuneStat +=1
+                            sickStat -=1;
+                        }, 5000)
+                        }
+                    }                        
+                }
+        }
+        for(let i = 0; i < this.touched.length; i ++){
+            for(let i = 0; i < touching.length; i ++){
+                if(touching[i] === dd){
+                    
+                }
             }
         }
+            
+
     }
+        
+
 }
